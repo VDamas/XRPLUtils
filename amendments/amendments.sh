94293f1c2f556d134e137721740a45234e4af34e
VDamas <vitoriodamas@gmail.com>
2020-04-17 15:07:55 +0100

    Update amendments.sh

--- amendments/amendments.sh
+++ amendments/amendments.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+# If running @wietse docker image leave it true
+incontainer=true
 rippledpath=/opt/ripple/bin/rippled
-amendmentspy=/opt/rippled/show_ammendments.py
-features=$(sudo docker exec rippledvalidator $rippledpath feature)
+
+# python scripts that converts json to table
+amendmentspy=/opt/rippled/ammendments.py
+
+if [ "$incontainer" = true ]
+then
+ features=$(sudo docker exec rippledvalidator $rippledpath feature)
+else
+ features=$(sudo $rippledpath feature)
+fi 
 python $amendmentspy "$features"
